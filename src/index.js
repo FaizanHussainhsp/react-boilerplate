9613228b8fe651d82bd9973706359723daccffa6
FaizanHussainhsp <faizanhussainhsp@gmail.com>
2023-03-13 22:29:01 +0500

    v

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "./themeconfig";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
 
---
+++ src/pages/home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MediaCard from "../../components/card";
+
+const Home = () => {
+  return (
+    <div>
+      <MediaCard />
+    </div>
+  );
+};
+
+export default Home;
