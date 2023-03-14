bf938516d9e076b75c83a563199466e23ee80adf
FaizanHussainhsp <faizanhussainhsp@gmail.com>
2023-03-14 09:42:17 +0500

    chnaged data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route } from "react-router-dom";
 import "./App.css";
+import { Button } from "./components/styledcomponents/button/index";
 import Home from "./pages/home";
 
 function App() {
   return (
     <div className="App">
+      <Button />
       <Route path="/" exact>
         <Home />
       </Route>
